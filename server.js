7e5848a05620a1df699840a0322b73c46954d38a
Ben Zeise <ben.zeise@gmail.com>
2017-10-02 10:29:17 -0500

    trying to get sql db on heroku

--- server.js
+++ server.js
@@ -6,13 +6,41 @@ var port = process.env.PORT || 1234;
 
 // globals
 var pg = require('pg');
-var config = {
-  database: 'tododb',
-  host: process.env.DATABASE_URL,
-  // host: process.env.DATABASE_URL || 'localhost',
-  // port: 5432, // always use this port for localhost postgresql
-  max: 12
-};
+// var config = {
+//   database: 'tododb',
+//   host: process.env.DATABASE_URL,
+//   // host: process.env.DATABASE_URL || 'localhost',
+//   // port: 5432, // always use this port for localhost postgresql
+//   max: 12
+// };
+
+var config = {};
+
+if (process.env.DATABASE_URL) {
+  // Heroku gives a url, not a connection object
+  // https://github.com/brianc/node-pg-pool
+  var params = url.parse(process.env.DATABASE_URL);
+
+  config = {
+    host: params.hostname,
+    port: params.port,
+    database: params.pathname.split('/')[1],
+    ssl: true, // heroku requires ssl to be true
+    max: 12, // max number of clients in the pool
+    idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+  };
+
+} else {
+  config = {
+    // user: process.env.PG_USER || null, //env var: PGUSER
+    // password: process.env.DATABASE_SECRET || null, //env var: PGPASSWOR
+    host: process.env.DATABASE_SERVER || 'localhost', // Server hosting the postgres database
+    port: process.env.DATABASE_PORT || 5432, //env var: PGPORT
+    database: process.env.DATABASE_NAME || 'tododb', //env var: PGDATABASE
+    max: 12, // max number of clients in the pool
+    idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+  };
+}
 
 var pool = new pg.Pool(config);
 
