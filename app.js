d61a28e480fcab5de74db09e9ba16aa75fbb9739
Ben Zeise <ben.zeise@gmail.com>
2017-10-01 21:38:48 -0500

    changes

--- app.js
+++ app.js
@@ -2,10 +2,7 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var bodyParser = require('body-parser');
-var urlencodedParser = bodyParser.urlencoded({
-  extended: false
-});
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 1234;
 
 // globals
 var pg = require('pg');
@@ -20,9 +17,7 @@ var pool = new pg.Pool(config);
 
 // static folder
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(bodyParser.urlencoded({extended: true}));
 
 // spin up server
 app.listen(port, function() {
@@ -35,80 +30,43 @@ app.get('/', function(req, res) {
   res.sendFile(path.resolve('public/views/index.html'));
 });
 
-// get to populate dropdown menu
-app.get('/dropdown', function(req, res) {
-  console.log('dropdown url hit');
-  pool.connect( function( err, connection, done ) {
-    if( err ) {
-      console.log( err );
-      done();
-      res.send( 400 );
-    } else {
-      console.log( 'connected to dropdown DB' );
-      var ownerNames = [];
-      var resultSet = connection.query( "SELECT owner_first, owner_last FROM petTable WHERE pet_name IS NULL GROUP BY owner_first, owner_last ORDER BY owner_last ASC;" );
-      resultSet.on('row', function(row) {
-        ownerNames.push(row);
-      }); //end
-      resultSet.on('end', function() {
-        done();
-        res.send(ownerNames);
-      });
-    } // end no error
-  }); // end pool connect
-});
-
-// post route
-app.post( '/register', function( req, res ) {
-  console.log( 'post hit to /register:', req.body );
-  pool.connect( function( err, connection, done ) {
-    if( err ){
-      console.log( err );
-      done();
-      res.send( 400 );
-    } else {
-      console.log( 'connected to db' );
-      connection.query( "INSERT INTO petTable ( owner_first, owner_last ) values ( $1, $2 )", [ req.body.firstName, req.body.lastName ] );
-      done();
-      res.send( 200 );
-    } // end no error
-  }); // end pool connect
-}); // end /images post
-
-app.post( '/petStuff', function( req, res ) {
-  console.log( 'post hit to /petStuff:', req.body );
+// post to add new task to taskDB
+app.post( '/tasks', function( req, res ) {
+  console.log( 'post hit to /tasks:', req.body );
   pool.connect( function( err, connection, done ){
     if( err ){
       console.log( err );
       done();
       res.send( 400 );
     } else {
-      console.log( 'connected to db' );
-      connection.query( "INSERT INTO petTable ( pet_name, pet_breed, pet_color, check_in ) values ( $1, $2, $3, $4 )", [ req.body.petName, req.body.breed, req.body.color, true ] );
+      console.log( 'connected to tasks db from post' );
+      connection.query( "INSERT INTO task_table (task) VALUES ( $1 )",
+      [ req.body.task ] );
       done();
       res.send( 200 );
     } // end no error
   }); // end pool connect
-}); // end /images post
+}); // end post
 
-app.post('/details', function(req, res) {
-  console.log('details url hit');
+// get to populate task list
+app.get('/tasks', function(req, res) {
+  console.log('get hit to /tasks');
   pool.connect( function( err, connection, done ) {
     if( err ) {
       console.log( err );
       done();
       res.send( 400 );
     } else {
-      console.log( 'connected to details DB' );
-      var petThang = [];
-      var resultSet = connection.query( 'SELECT ( owner_fullname, pet_name, pet_breed, pet_color) from petTable WHERE owner_first is null' );
+      console.log( 'connected to tasks DB from get' );
+      var taskList = [];
+      var resultSet = connection.query( "SELECT * FROM task_table ORDER BY complete, task" );
       resultSet.on('row', function(row) {
-        petThang.push(row);
+        taskList.push(row);
       }); //end
       resultSet.on('end', function() {
         done();
-        console.log(petThang);
-        res.send(petThang);
+        console.log(taskList);
+        res.send(taskList);
       });
     } // end no error
   }); // end pool connect
